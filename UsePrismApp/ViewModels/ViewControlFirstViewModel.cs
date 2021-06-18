1411ed535b2baab6c138dfbd84e093c869fda065
shimanamisan <mikan.sup.all@gmail.com>
2021-06-19 06:26:19 +0900

    [update] 純正のメッセージボックスを表示するコードを追加

--- UsePrismApp/ViewModels/ViewControlFirstViewModel.cs
+++ UsePrismApp/ViewModels/ViewControlFirstViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Prism.Services.Dialogs;
 using UsePrismApp.Views; // nameof(ViewControlSecond)とキーに画面を指定するときに必要
+using UsePrismApp.Services;
 
 namespace UsePrismApp.ViewModels
 {
@@ -16,6 +17,8 @@ namespace UsePrismApp.ViewModels
 
         private IDialogService _dialogService;
 
+        private IMessageService _messageService;
+
         private string _labelA = string.Empty;
         public string LabelA
         {
@@ -23,14 +26,30 @@ namespace UsePrismApp.ViewModels
             set { SetProperty(ref _labelA, value); }
         }
 
-        public ViewControlFirstViewModel(IDialogService dialogService)
+        // dialogService だけ受けて、thisで2つ目のコンストラクタの ViewControlFirstViewModel に dialogService と
+        // インスタンス化した MessageService を渡している
+        public ViewControlFirstViewModel(IDialogService dialogService) : this(dialogService, new MessageService())
+        {
+            // thisを指定することで複数のコンストラクタを呼び出すことができる
+        }
+
+        // 自分で作ったインターフェースをコンストラクタに渡した場合に、Prism側で理解できないのでエラーになる
+        // IMessageService を実装した MessageService をインスタンス化したものを、1つ目のコンストラクタで指定して
+        // こっちの（2つ目の）コンストラクタで受け取るように実装している
+        public ViewControlFirstViewModel(
+            IDialogService dialogService,
+            IMessageService messageService)
         {
             ViewFirstControlOKButton = new DelegateCommand(ViewFirstControlOKButtonExecute);
+            ViewFirstControlOKButton2 = new DelegateCommand(ViewFirstControlOKButtonExecute2);
 
             _dialogService = dialogService;
+
+            _messageService = messageService;
         }
 
         public DelegateCommand ViewFirstControlOKButton { get; }
+        public DelegateCommand ViewFirstControlOKButton2 { get; }
 
         // 画面遷移で通過するときの処理
         public void OnNavigatedTo(NavigationContext navigationContext)
@@ -62,6 +81,16 @@ namespace UsePrismApp.ViewModels
             var p = new DialogParameters();
             p.Add(nameof(ViewControlSecondViewModel.ViewControlSecondTextBox), "Saveします");
             _dialogService.ShowDialog(nameof(ViewControlSecond), p, null);
+        }
+
+        // 標準のメッセージボックスを表示させてテストする方法
+        private void ViewFirstControlOKButtonExecute2()
+        {
+            // インターフェース越しに純正のメッセージボックスを出すパターン
+            if (_messageService.Question("Saveしますか？") == MessageBoxResult.OK)
+            {
+                _messageService.ShowDialog("Saveしました");
+            }
         }
     }
 }
