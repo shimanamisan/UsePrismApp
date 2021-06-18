f01bc84df9342a01ec53e87cdcf43089af8dd5bf
shimanamisan <mikan.sup.all@gmail.com>
2021-06-19 06:25:10 +0900

    [add] 純正のメッセージボックスを使用してテストを実行する為にインターフェースを追加

---
+++ UsePrismApp/Services/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UsePrismApp.Services
+{
+    public interface IMessageService
+    {
+        // OKボタンだけのメッセージボックス
+        void ShowDialog(string message);
+
+        // 質問系のメッセージボックス
+        MessageBoxResult Question(string message);
+    }
+}
---
+++ UsePrismApp/Services/MessageService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UsePrismApp.Services
+{
+    public sealed class MessageService : IMessageService
+    {
+        public MessageBoxResult Question(string message)
+        {
+            return MessageBox.Show(message, "問い合わせ", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+        }
+
+        public void ShowDialog(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
